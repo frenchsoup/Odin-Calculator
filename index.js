3b812352eb96121a3ec9743ad90cfd3c3111aa20
Alex LeFevre <alexander.m.lefevre@gmail.com>
2022-02-04 22:38:28 -0500

    removed unreachable breaks

--- index.js
+++ index.js
@@ -18,16 +18,12 @@ function operate(operator, num1, num2) {
   switch (operator) {
     case '+':
       return add(num1, num2);
-      break;
     case '-':
       return subtract(num1, num2);
-      break;
     case '*':
       return multiply(num1, num2);
-      break;
     case '/':
       return divide(num1, num2);
-      break;
     default:
       break;
   }
